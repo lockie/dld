658417848482fb26c347c16da9d49258d5ffe5a6
Andrew Kravchuk <awkravchuk@gmail.com>
2020-07-14 07:56:59 +0300

    added aggressive compiler optimization settings

--- build.lisp
+++ build.lisp
@@ -1,4 +1,4 @@
-(push :release *features*)
+(proclaim '(optimize (speed 3) (safety 0) (debug 0) (compilation-speed 0) (space 0)))
 (ql:quickload '(:d2clone-kit :deploy))
 (push (truename ".") asdf:*central-registry*)
 (asdf:make :dld)
